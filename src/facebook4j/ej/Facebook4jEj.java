5104d0fca55a694e984cd6445d315191b7563631
igonzalezcastro <igonzalezcastro@atanasoff21.danielcastelao.org>
2016-05-18 11:45:31 +0200

    Facebook4j

---
+++ src/facebook4j/ej/Facebook4jEj.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facebook4j.ej;
+
+
+
+/**
+ *
+ * @author igonzalezcastro
+ */
+public class Facebook4jEj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Interfaz ventana = new Interfaz();
+       ventana.setVisible(true);
+      
+    }
+
+}
